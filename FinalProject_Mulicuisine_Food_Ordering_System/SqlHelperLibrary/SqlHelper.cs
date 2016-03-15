8016126c17acd6be85de8c719ebc0ea5a70abdcf
sushilpatil1992 <sushilpatil1992@gmail.com>
2016-03-15 11:59:44 +0530

    Multicuisine Ordering System
    Multicuisine Ordering System

---
+++ FinalProject_Mulicuisine_Food_Ordering_System/SqlHelperLibrary/SqlHelper.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlHelperLibrary
+{
+    public class SqlHelper : IDisposable, ISqlHelper
+    {
+        private SqlCommand command;
+        private SqlConnection connection;
+
+        private SqlHelper()
+        {        }
+        public SqlHelper(string conString)
+        {
+            ConnectionString = conString;
+        }
+        public String ConnectionString
+        {
+            get { return ConnectionString; }
+            set { ConnectionString = value; }
+        }
+
+        public int ExecuteNonQuery(CommandType cmdType, string cmdText, params SqlParameter[] cmdParms)
+        {
+            SqlCommand cmd = new SqlCommand();
+
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                PrepareCommand(cmd, conn, null, cmdType, cmdText, cmdParms);
+                int val = cmd.ExecuteNonQuery();
+                cmd.Parameters.Clear();
+                return val;
+            }
+        }
+
+        public SqlDataReader ExecuteReader(string cmdText, CommandType cmdType, params SqlParameter[] cmdParameters)
+        {
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+                SqlCommand cmd = new SqlCommand(cmdText, con);
+                SqlDataReader reader = cmd.ExecuteReader();
+                return reader;
+            }
+        }
+
+        public object ExecuteScalar(string cmdText, CommandType cmdType, params SqlParameter[] cmdParameters)
+        {
+            if (connection.State != ConnectionState.Open)
+                connection.Open();
+            SqlCommand cmdSql = new SqlCommand(cmdText, connection);
+            return cmdSql.ExecuteScalar();
+        }
+
+        private void PrepareCommand(SqlCommand cmd, SqlConnection conn, SqlTransaction trans, CommandType cmdType, string cmdText, SqlParameter[] cmdParms)
+        {
+            if (conn.State != ConnectionState.Open)
+                conn.Open();
+            cmd.Connection = conn;
+            cmd.CommandText = cmdText;
+            if (trans != null)
+                cmd.Transaction = trans;
+
+            cmd.CommandType = cmdType;
+
+            if (cmdParms != null)
+            {
+                foreach (SqlParameter parm in cmdParms)
+                    cmd.Parameters.Add(parm);
+            }
+        }
+
+        public void Dispose()
+        {
+            command.Dispose();
+            connection.Dispose();
+        }
+    }
+ }
---
+++ FinalProject_Mulicuisine_Food_Ordering_System/TestConsoleApplication/Program.cs
@@ -0,0 +1,53 @@
+﻿using SqlHelperLibrary;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String conString=@"Data Source=(LocalDB)\v11.0;AttachDbFilename=E:\Dac\Project\FinalProject_Mulicuisine_Food_Ordering_System\TestConsoleApplication\TestDb.mdf;Integrated Security=True";
+            SqlHelper helper = new SqlHelper(conString);
+            SqlConnection con = new SqlConnection();        
+            string cmdStr = "SELECT * FROM Table";
+            //Create Command Object
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = cmdStr;
+            //associate command with connection
+            cmd.Connection = con;
+            try
+            {
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    int cutomerId = int.Parse(dr["id"].ToString());
+                    string firstName = dr["firstname"].ToString();
+                    string lastName = dr["lastname"].ToString();
+                    string email = dr["email"].ToString();
+                    string phone = dr["phoneno"].ToString();
+                    DateTime bDate = Convert.ToDateTime(dr["birthdate"]);
+                    string address = dr["address"].ToString();
+                    Console.WriteLine(firstName + " " + lastName + "  " + email + "  " + phone + "  " + address + " " + bDate.ToShortDateString());
+                }
+
+            }
+            catch (OleDbException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
